e30260a5b01454c51d5f2b68c75b05e38e77f555
10Hyuna <kimha105@naver.com>
2024-10-29 14:16:51 +0900

    [Style] main function 내용 삭제

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import view.Stage;
-
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Stage stage = new Stage();
-        });
+
     }
 }
