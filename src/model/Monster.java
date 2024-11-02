3723f7b2ee543b0ced1497a40c33fd057c481dfa
hyeyeon <shymirr20@gmail.com>
2024-11-02 17:26:03 +0900

    Monster 객체

---
+++ src/model/Monster.java
@@ -0,0 +1,15 @@
+package model;
+
+public abstract class Monster {
+
+    public int hp;
+    public Monster(int hp) {
+        this.hp = hp;
+    }
+
+    public void move(){}
+
+    protected void resetHp(int hp)  {
+        this.hp = hp;
+    }
+}
