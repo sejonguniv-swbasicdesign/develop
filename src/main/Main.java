317867ef92017982d0d478b443e534c553c1059f
hyeyeon <shymirr20@gmail.com>
2024-11-27 15:32:49 +0900

    메인 화면으로 실행되게 수정

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,10 @@
 package main;
 
 import view.container.frame.MainFrame;
-import view.container.frame.third.InitBossStage;
+
 
 public class Main {
     public static void main(String[] args) {
-//        new MainFrame();
-        new InitBossStage();
+        new MainFrame();
     }
 }
